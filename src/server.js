2e4ab2c00c9357707d0839190b46d00210c049a4
Cong Duan <phamcongduannn@gmail.com>
2025-10-22 12:17:21 +0700

    config static file

--- src/server.js
+++ src/server.js
@@ -6,9 +6,13 @@ const path = require('path')
 const app = express() //app express
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME
+
 //config template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+//config static file
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Khai báo route
 app.get('/', (req, res) => {
