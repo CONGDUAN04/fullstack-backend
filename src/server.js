e0d068ccd4f86e859ffa3224076ea4c4a8ccca72
Cong Duan <phamcongduannn@gmail.com>
2025-10-22 22:26:58 +0700

    connection db

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express') //commonjs
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
+// Get the client
 const app = express() //app express\
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME
@@ -9,6 +11,16 @@ const hostname = process.env.HOST_NAME
 configViewEngine(app)
 //Khai báo route 
 app.use('/', webRoutes)
+//test connection
+
+// A simple SELECT query
+connection.query(
+    'select * from Users',
+    function (err, results, fields) {
+        console.log("check results:", results)
+        console.log("check fields:", fields)
+    }
+)
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
