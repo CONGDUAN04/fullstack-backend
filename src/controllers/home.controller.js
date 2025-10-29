30aa1f431a30af37cf94a8b63aa5d90355e81a63
Cong Duan <phamcongduannn@gmail.com>
2025-10-29 11:53:38 +0700

    CRUD user

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -19,21 +19,18 @@ const getCreatePage = (req, res) => {
 }
 const getUpdatePage = async (req, res) => {
     const id = req.params.id
-    const user = await getUserById(id)
+    const user = await User.findById(id).exec();
     res.render('edit.ejs', { user: user }) // x <- y
 }
 const postUpdateUser = async (req, res) => {
     const userId = req.body.userId
     const { email, name, city } = req.body
-    await updateUserById(email, city, name, userId)
+    await User.updateOne({ _id: userId }, { email, name, city })
     res.redirect('/')
 }
 const postDeleteUser = async (req, res) => {
     const id = req.params.id
-    console.log("Check id", id)
-    const [result, fields] = await connection.query(
-        `DELETE FROM Users WHERE id = ?`, [id]
-    )
+    await User.deleteOne({ _id: id });
     res.redirect('/')
 }
 module.exports = {
